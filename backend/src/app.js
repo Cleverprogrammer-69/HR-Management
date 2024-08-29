f611f4de3f2269cf14eb81f383d98183d3b722ec
Hafiz Muahammad Abdullah <76812283+Cleverprogrammer-69@users.noreply.github.com>
2024-08-29 17:15:25 +0500

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieParser from 'cookie-parser'
 const app = express()
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: "*",
     credentials: true
 }))
 app.use(express.json({limit: "16kb"}))
