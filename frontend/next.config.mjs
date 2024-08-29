63ba4ff988fccd957acaa1d5889282abe179d9c5
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-08-29 16:19:50 +0500

    server link is changed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,7 +2,7 @@
 // require('dotenv').config({ path: './.env' });
 const nextConfig = {
   env: {
-    HR_API_V1: 'https://hr-management-server.onrender.com',
+    HR_API_V1: 'https://hr-management-server.onrender.com/api/v1',
   },
   images: {
     remotePatterns: [
