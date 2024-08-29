d2565313787a138a4224277b9cc1002cdcff6e32
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-08-29 17:54:24 +0500

    updated middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,30 +1,32 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { NextURL } from 'next/dist/server/web/next-url';
 import jwt from 'jsonwebtoken';
-import path from 'path';
+
 export async function middleware(req: NextRequest) {
-  const { pathname, origin } = req.nextUrl;
+  const { pathname } = req.nextUrl;
   const accessToken = req.cookies.get('accessToken')?.value;
   const isAccessTokenValid = accessToken ? decodeToken(accessToken) : false;
+
   if (!isAccessTokenValid) {
-    // // If the token is invalid and user already on the login page,
-    // // redirect to /login
+    // If the token is invalid and user is not on an auth page, redirect to /login
     const authUrls = ['/login', '/signup'];
     if (!authUrls.includes(pathname)) {
-      const authUrl = new NextURL('/login', origin);
-      return NextResponse.redirect(authUrl);
+      const loginUrl = req.nextUrl.clone();
+      loginUrl.pathname = '/login';
+      return NextResponse.redirect(loginUrl);
     }
   } else {
-    // // If token is valid and trying to access login, redirect to home
+    // If token is valid and trying to access login or signup, redirect to home
     if (pathname === '/login' || pathname === '/signup') {
-      const homeUrl = new NextURL('/', origin);
+      const homeUrl = req.nextUrl.clone();
+      homeUrl.pathname = '/';
       return NextResponse.redirect(homeUrl);
     }
   }
 
   return NextResponse.next();
 }
+
 export const config = {
   matcher: [
     '/signup',
@@ -42,6 +44,7 @@ export const config = {
     '/jobNature/:path*',
   ],
 };
+
 function decodeToken(token: string): boolean {
   try {
     const decodedToken = jwt.decode(token) as jwt.JwtPayload;
