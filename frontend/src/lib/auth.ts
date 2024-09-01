cb076babb65847679068d7cfa052e1ec4089ce91
Cleverprogrammer-69 <hafizabdullah376@gmail.com>
2024-09-01 23:49:11 +0500

    Some routing issues testing

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,8 +1,6 @@
-import type { NextRequest, NextResponse } from 'next/server';
-import { SignJWT, jwtVerify } from 'jose';
+import type { NextRequest } from 'next/server';
+import {jwtVerify } from 'jose';
 import { USER_TOKEN, getJwtSecretKey } from './constants';
-import { StringHeaderIdentifier } from '@tanstack/react-table';
-
 interface UserJwtPayload {
   jti: string;
   iat: number;
@@ -10,18 +8,16 @@ interface UserJwtPayload {
 
 export class AuthError extends Error {}
 
-export async function verifyAuth(token: string) {
-  // const token = req.cookies.get(USER_TOKEN)?.value;
+export async function verifyAuth(req: NextRequest) {
+  const token = req.cookies.get(USER_TOKEN)?.value;
 
   if (!token) throw new AuthError('Missing user token');
-  console.log("verifyAuth triggered")
+
   try {
     const verified = await jwtVerify(
       token,
-      new TextEncoder().encode(getJwtSecretKey()),
-      {}
+      new TextEncoder().encode(getJwtSecretKey())
     );
-    console.log('Verified ? :', verified.payload as UserJwtPayload);
     return verified.payload as UserJwtPayload;
   } catch (err) {
     throw new AuthError('Your token has expired.');
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,33 +1,33 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import { verifyAuth } from './lib/auth';
-import { cookies } from 'next/headers';
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
   const isPublicPath = path === '/login' || path === '/signup'
-  const accessToken = cookies().get('accessToken')?.value;
   console.log("Cookies in middleware", req.cookies.getAll())
   console.log(req.headers)
   console.log('Middleware triggered');
   console.log('Pathname:', path);
-  console.log('AccessToken:', accessToken ? 'Present' : 'Absent');
 
-  const isAccessTokenValid = accessToken ? await verifyAuth(accessToken).catch(err=>{
+  const isAccessTokenValid = await verifyAuth(req).catch(err=>{
     console.log(err)
-  }) : null;
+  });
   console.log('Is Access Token Valid:', isAccessTokenValid);
-  if(isPublicPath && isAccessTokenValid){
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
-  if(!isPublicPath && !isAccessTokenValid){
-    return NextResponse.redirect(new URL('/login', req.url));
+  if (!isAccessTokenValid) {
+    // if this an API request, respond with JSON
+    if (!isPublicPath) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+    // otherwise, redirect to the set token page
+    // else {
+    //   return NextResponse.redirect(new URL('/', req.url));
+    // }
   }
 
  
 
   console.log('Proceeding with request');
-  return NextResponse.next();
+  // return NextResponse.next();
 }
 
 export const config = {
